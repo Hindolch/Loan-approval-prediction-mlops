3f3811de11c5b51180850a00c881e7c6faf4b39b
Hindol Roy Choudhury <roychoudhuryhindol@gmail.com>
2024-12-20 16:39:06 +0530

    another commit

--- tests/test_inferencing.py
+++ tests/test_inferencing.py
@@ -1,21 +1,26 @@
 import joblib
 import pandas as pd
 import pytest
+import os, sys
 
-input_df = pd.DataFrame([{
-     'Dependents': 3,
-    'ApplicantIncome': 5674,
-    'CoapplicantIncome': 4996,
-    'LoanAmount': 898,
-    'Loan_Amount_Term': 360,
-    'Credit_History': 1,
-    'Property_Area': 2,
-    'Gender_Male': 1,
-    'Married_Yes': 1,
-    'Education_Not Graduate': 1,
-    'Self_Employed_Yes': 1
-    
-}])
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+
+@pytest.fixture
+def input_df():
+    return pd.DataFrame([{
+        'Dependents': 3,
+        'ApplicantIncome': 5674,
+        'CoapplicantIncome': 4996,
+        'LoanAmount': 898,
+        'Loan_Amount_Term': 360,
+        'Credit_History': 1,
+        'Property_Area': 2,
+        'Gender_Male': 1,
+        'Married_Yes': 1,
+        'Education_Not Graduate': 1,
+        'Self_Employed_Yes': 1
+    }])
 
 model_path = 'Artifacts/model.pkl'
 model = joblib.load(model_path)
